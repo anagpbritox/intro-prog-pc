1a07898baad8404220017c6c800ff989f8fc4a41
anagpbritox <ana.giovanna@escolar.ifrn.edu.br>
2025-09-17 19:56:09 -0300

    Create V21.py

---
+++ V21.py
@@ -0,0 +1,25 @@
+# @cikey b8c2f0e997296c4047aa90efbb14a388
+# @sid 20251174010038
+# @aid V2.1
+
+
+#begin_inputs
+
+# defina as variáveis aqui
+
+x=1
+y=2
+a=x
+b=y
+c=3
+d=4
+
+#end_inputs
+
+
+#os prints vão a partir daqui
+
+- print("{} e {} sao {} e {}".format("x, y, a, b"))
+- print("Total: {} + {}".format("x, y"))
+- print("{}, {}, {}, são números inteiros".format("a, b, c"))
+- print("{}, + 0 =".format("z"))
