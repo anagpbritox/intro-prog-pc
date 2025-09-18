2e479de80a323902b855754e59e2268adf4adbca
anagpbritox <ana.giovanna@escolar.ifrn.edu.br>
2025-09-18 02:34:16 -0300

    Create V22.py

---
+++ V22.py
@@ -0,0 +1,14 @@
+# @cikey b8c2f0e997296c4047aa90efbb14a388
+# @sid 20251174010038
+# @aid V2.2
+
+
+#begin_inputs
+
+dia = 27
+mes = 4
+ano = 2009
+
+#end_inputs
+
+print("{}/{}/{}".format(dia,mes,ano))
