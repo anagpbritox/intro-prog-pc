840592f56d2afcb59b11f35d8fbefaaf798ffdf4
anagpbritox <ana.giovanna@escolar.ifrn.edu.br>
2025-09-18 20:48:56 -0300

    Update V21.py

--- V21.py
+++ V21.py
@@ -19,7 +19,7 @@ y = b
 
 #os prints vão a partir daqui
 
-- print("{} e {} são {} e {}".format(x,y,a,b))
-- print("Total: {} + {}".format(x,y))
-- print("{} {} {} são números inteiros".format(a,b,c))
-- print("{} + 0 = {}".format(z,z+0))
+print("{} e {} são {} e {}".format(x,y,a,b))
+print("Total: {} + {}".format(x,y))
+print("{} {} {} são números inteiros".format(a,b,c))
+print("{} + 0 = {}".format(z,z+0))
