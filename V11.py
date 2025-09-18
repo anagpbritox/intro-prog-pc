a19d9e2b3f4885f212ea05140c7744d9a8f67a02
anagpbritox <ana.giovanna@escolar.ifrn.edu.br>
2025-09-18 02:39:51 -0300

    Create V11.py

---
+++ V11.py
@@ -0,0 +1,35 @@
+# @cikey b8c2f0e997296c4047aa90efbb14a388
+# @sid 20251174010038
+# @aid V1.1
+
+
+#begin_inputs
+
+num6=6
+num4=4
+num20=20
+
+# As variáveis de entrada (os numeros, neste caso) deverão ser definidos nessa área 
+# entre #begin_inputs e #end_inputs.
+
+# O restante do código deve estar após o #end_inputs
+
+#end_inputs
+
+soma=num6+num4
+print(soma)
+
+subtração=num4-num6
+print(subtração)
+
+multiplicação=num6*num4
+print(multiplicação)
+
+divisão=num20/num6
+print(divisão)
+
+resto_da_divisão=num20//num6
+print(resto_da_divisão)
+
+resto_da_divisão_inteira=num20 % num6
+print(resto_da_divisão_inteira)
