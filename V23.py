5f1c55c2cc3ddefd7c08b9dc0d1076033e4afff0
anagpbritox <ana.giovanna@escolar.ifrn.edu.br>
2025-09-18 02:34:20 -0300

    Create V23.py

---
+++ V23.py
@@ -0,0 +1,14 @@
+# @cikey b8c2f0e997296c4047aa90efbb14a388
+# @sid 20251174010038
+# @aid V2.3
+
+
+#begin_inputs
+
+a = int(input("Digite a sua primeira nota:"))
+b = int(input("Digite a sua segunda nota:"))
+c = int(input("Digite a sua terceira nota:"))
+
+#end_inputs
+
+print("{}".format((a+b+c)/3))
