74a54631e7e53119e4b8bed6a2f5df6183fbc9aa
anagpbritox <ana.giovanna@escolar.ifrn.edu.br>
2025-09-18 02:37:41 -0300

    Create V24.py

---
+++ V24.py
@@ -0,0 +1,10 @@
+# @cikey b8c2f0e997296c4047aa90efbb14a388
+# @sid 20251174010038
+# @aid V2.4
+
+
+#begin_inputs
+
+valor_compra = int(input("Digite o valor da sua primeira compra:"))
+
+#end_inputs
