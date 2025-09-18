c6ae6843259e528f5b0279fc2b24fe7fb9521d10
anagpbritox <ana.giovanna@escolar.ifrn.edu.br>
2025-09-18 20:04:51 -0300

    Update V11.py

--- V11.py
+++ V11.py
@@ -5,9 +5,9 @@
 
 #begin_inputs
 
-num6=6
-num4=4
-num20=20
+num6 = 6
+num4 = 4
+num20 = 20
 
 # As variáveis de entrada (os numeros, neste caso) deverão ser definidos nessa área 
 # entre #begin_inputs e #end_inputs.
@@ -16,19 +16,19 @@ num20=20
 
 #end_inputs
 
-soma=num6+num4
+soma = num6 + num4
 print(soma)
 
-subtração=num4-num6
+subtração = num4 - num6
 print(subtração)
 
-multiplicação=num6*num4
+multiplicação = num6 * num4
 print(multiplicação)
 
-divisão=num20/num6
+divisão = num20 / num6
 print(divisão)
 
-resto_da_divisão=num20//num6
+resto_da_divisão = num20 // num6
 print(resto_da_divisão)
 
 resto_da_divisão_inteira=num20 % num6
