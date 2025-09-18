675fa99a126b21a5fcf15179958bf61a37abbd6f
anagpbritox <ana.giovanna@escolar.ifrn.edu.br>
2025-09-18 20:48:59 -0300

    Update V24.py

--- V24.py
+++ V24.py
@@ -5,6 +5,17 @@
 
 #begin_inputs
 
-valor_compra = int(input("Digite o valor da sua primeira compra:"))
+valor_compra = int(input("Digite o valor da compra: "))
 
 #end_inputs
+
+à_vista = valor_compra * 0.09
+
+parcelado_5 = valor_compra / 5
+
+total_juros = valor_compra * 1.17
+parcelado_10 = total_juros / 10
+
+print("{}".format(valor_compra - à_vista))
+print("{}".format(parcelado_5))
+print("{}".format(round(parcelado_10, 2)))
