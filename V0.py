766dfdc37bef0a791f6555bda98378805ddada4b
anagpbritox <ana.giovanna@escolar.ifrn.edu.br>
2025-09-18 22:11:23 -0300

    Create V0.py

---
+++ V0.py
@@ -0,0 +1,19 @@
+# @cikey b8c2f0e997296c4047aa90efbb14a388
+# @sid 20251174010038
+# @aid 0
+
+
+#begin_inputs
+
+num1 = 2
+num2 = 2
+
+# Defina as variáveis de entrada aqui
+# Não apague as linhas com #begin_inputs e #end_inputs
+
+#end_inputs
+
+# Faça o processamento e a exibição da saída a partir daqui
+	
+soma = num1 + num2
+print(soma)
