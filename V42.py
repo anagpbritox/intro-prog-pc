8cbbeab6ffa3e03c26e2220293c0c772f3351eba
anagpbritox <ana.giovanna@escolar.ifrn.edu.br>
2025-09-23 20:17:59 -0300

    Create V42.py

---
+++ V42.py
@@ -0,0 +1,22 @@
+# @cikey b8c2f0e997296c4047aa90efbb14a388
+# @sid 20251174010038
+# @aid V4.2
+
+
+#begin_inputs
+
+#mantenha esse trecho. o input será manipulado aqui.
+
+p1 = int(input("Digite a sua primeira nota: "))
+p2 = int(input("Digite a sua primeira nota: "))
+p3 = int(input("Digite a sua primeira nota: "))
+p4 = int(input("Digite a sua primeira nota: "))
+p5 = int(input("Digite a sua primeira nota: "))
+p6 = int(input("Digite a sua primeira nota: "))
+p7 = int(input("Digite a sua primeira nota: "))
+p8 = int(input("Digite a sua primeira nota: "))
+p10 = int(input("Digite a sua primeira nota: "))
+
+#end_inputs
+
+print()
