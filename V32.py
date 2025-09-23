eca5b77a17ec180899a81416c1883e30087d1514
anagpbritox <ana.giovanna@escolar.ifrn.edu.br>
2025-09-23 20:17:43 -0300

    Create V32.py

---
+++ V32.py
@@ -0,0 +1,16 @@
+# @cikey b8c2f0e997296c4047aa90efbb14a388
+# @sid 20251174010038
+# @aid V3.2
+
+
+#begin_inputs
+
+# Isso está aqui apenas para grantir que os testes automáticos funcionem
+# NÃO mexa aqui.
+
+#end_inputs
+
+for i in range(99, 251):
+    print(i, "bugs no software, pegue um deles e conserte...")
+    print("Tente Ctrl+5")
+print("Vamos tomar mais um cafe!")
