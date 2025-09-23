1c45d073c126313d80114a6520c76c91e740a2f9
anagpbritox <ana.giovanna@escolar.ifrn.edu.br>
2025-09-23 20:17:55 -0300

    Create V41.py

---
+++ V41.py
@@ -0,0 +1,17 @@
+# @cikey b8c2f0e997296c4047aa90efbb14a388
+# @sid 20251174010038
+# @aid V4.1
+
+
+#begin_inputs
+
+x = int(input("Digite a sua primeira nota: "))
+y = int(input("Digite a sua primeira nota: "))
+
+#end_inputs
+
+if x > y:
+    print(x)
+
+if x < y:
+    print(y)
