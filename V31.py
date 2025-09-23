e17423c2e5510358d29e0ab8e94a2bf09173dc2d
anagpbritox <ana.giovanna@escolar.ifrn.edu.br>
2025-09-23 20:17:40 -0300

    Create V31.py

---
+++ V31.py
@@ -0,0 +1,14 @@
+# @cikey b8c2f0e997296c4047aa90efbb14a388
+# @sid 20251174010038
+# @aid V3.1
+
+
+#begin_inputs
+
+# Isso está aqui apenas para grantir que os testes automáticos funcionem
+# NÃO mexa aqui.
+
+#end_inputs
+
+for i in range(99, 251):
+    print(i, "bugs no software, pegue um deles e conserte...")
